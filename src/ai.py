fd41266eb7f86eae391691b310344974133c8133
Simen Gangstad <simen.gangstad@me.com>
2020-08-25 10:50:50 +0200

    Migrate all nodes to python

--- src/ai.py
+++ src/ai.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
 
 import rospy
+from geometry_msgs.msg import Polygon, PoseStamped, Point
+
+# Set up callback here
+boxes = Polygon()
+def boxesCallback(message):
+    global boxes
+    boxes = message
 
 
-# Initialize ROS
-rospy.init_node("ai_node")
+drone_position = Point()
+def poseCallback(message):
+    global drone_position 
+    drone_position = message.pose.position
 
 
-# Set up subscribers here
+def main():
+
+    # Initialize ros
+    rospy.init_node("ai_node")
 
 
+    # Set up subscribers here
+    rospy.Subscriber("/perception/boxes", Polygon, boxesCallback)
+    rospy.Subscriber("/mavros/local_position/pose", PoseStamped, poseCallback)
+
+    # Set up publisher here
 
 
-# Main loop
-rospy.loginfo("ai is running")
-rate = rospy.Rate(30)
-while not rospy.is_shutdown():
+    # Main loop
+    rospy.loginfo("AI is running")
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        rospy.loginfo(boxes)
 
 
-    # Rate limiting
-    rate.sleep()
+        # Rate limiting
+        rate.sleep()
 
-
-
-
-
+if __name__ == '__main__':
+    # This is just here so ROS doesn't throw a warning when we close the nodes in the terminal
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
 
 
 
---
+++ src/control.py.bak
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import rospy
+
+# Initialize ROS
+rospy.init_node("control_node")
+
+rospy.loginfo("Control is running")
+
