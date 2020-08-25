9cda440b49efe43a5382d03c5bcf4696786a49a8
Simen Gangstad <simen.gangstad@me.com>
2020-08-25 10:51:15 +0200

    Add setup script for dependencies

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install ROS and setup sourcing 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install ros-noetic-desktop-full -y
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source /opt/ros/noetic/setup.bash
+
+# Install Catkin and extra dependencies
+sudo apt install python3-pip python3-catkin-tools ros-noetic-mavros-msgs -y
+pip3 install osrf-pycommon
