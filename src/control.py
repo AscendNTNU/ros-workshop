dede527d7785c319c7607dd9c9bd4cc687e87513
Simen Gangstad <simen.gangstad@me.com>
2020-09-16 12:13:34 +0200

    Move solutions up to src

--- src/control.py
+++ src/control.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import Point, PoseStamped
 from mavros_msgs.msg import PositionTarget
+from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
 from ros_workshop.srv import FlyCommand, FlyCommandRequest, FlyCommandResponse
 
 
@@ -13,14 +14,27 @@ def poseCallback(message):
     got_drone_position = True
     drone_position = message.pose.position
 
+
+# Functions for controling the drone
 setpoint = PositionTarget() 
 
-
 # Write your service function for take off here
+def handleTakeOffRequest(request):
+    response = TriggerResponse()	
+    
+    rospy.loginfo("Handling take off request")
+    # If the drone is at ground
+    if drone_position.z <= 0.1:
+        response.success = True
+        response.message = "All ok!"
+    
+        # Set the take off position to 3 m in air 
+        setpoint.position.z = 3.0
+    else:
+        response.success = False 
+        response.message = "Seems like the drone is already flying!"
 
-
-
-
+    return response
 
 def handleFlyRequest(request):
     response = FlyCommandResponse()	
@@ -53,7 +67,7 @@ def main():
     setpoint_publisher = rospy.Publisher("/drone/setpoint", PositionTarget, queue_size = 1)
 
     # Write service handlers here
-
+    take_off_service = rospy.Service("/control/take_off", Trigger, handleTakeOffRequest)
     fly_command_service = rospy.Service("/control/fly", FlyCommand, handleFlyRequest)
     
     # Main loop
--- src/perception.py
+++ src/perception.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
 
 import rospy
+from geometry_msgs.msg import PoseArray, Polygon
 
 # Set up callback here
+boxes_message = PoseArray()
+def callback(message):
+    global boxes_message
+    boxes_message = message
 
-# Initialize ROS 
-rospy.init_node("perception_node")
+def main():
 
-
-# Set up subscribers here
-
-# Main loop
-rospy.loginfo("Perception is running")
-rate = rospy.Rate(30)
-while not rospy.is_shutdown():
+    # Initialize ROS
+    rospy.init_node("perception_node")
 
 
-    # Rate limiting
-    rate.sleep()
+    # Set up subscriber here 
+    rospy.Subscriber("/simulator/boxes", PoseArray, callback)
+
+    # Set up publisher here
+    publisher = rospy.Publisher("/perception/boxes", Polygon, queue_size=1)
+
+    # Main loop
+    rospy.loginfo("Perception is running")
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+
+        polygon_message = Polygon()
+        for pose in boxes_message.poses:
+            point = pose.position
+            polygon_message.points.append(point)
+
+        publisher.publish(polygon_message)
 
 
+        # Rate limiting
+        rate.sleep()
 
-
-
-
-
+if __name__ == '__main__':
+    # This is just here so ROS doesn't throw a warning when we close the nodes in the terminal
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
 
 
